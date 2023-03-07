810a843627ea5fb06f28b275fc1f7e0c1074750c
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-03-07 21:38:39 +0200

    k

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,28 @@
 // Add imports above this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
+const gallery = document.querySelector(`.gallery`);
+//
+const addGallaryItem = images => {
+  return images
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href=${original}>
+          <img
+            class="gallery__image"
+            src=${preview}
+            alt=${description}
+          />
+        </a>`;
+    })
+    .join(``);
+};
 
-console.log(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, addGallaryItem(galleryItems));
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
