10afba5d5fde9a22f6fe488d8629f7af42b83a31
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-03-08 10:08:23 +0200

    + video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -10,13 +10,15 @@ const gallery = document.querySelector(`.gallery`);
 const addGallaryItem = images => {
   return images
     .map(({ preview, original, description }) => {
-      return `<a class="gallery__item" href=${original}>
-          <img
-            class="gallery__image"
-            src=${preview}
-            alt=${description}
-          />
-        </a>`;
+      return `<div>
+      <a class="gallery__item" href="${original}">
+       <img
+        class="gallery__image"
+         src="${preview}"
+         alt="${description}"
+       />
+      </a>
+     </div>`;
     })
     .join(``);
 };
