43ae1439d0469a215f1ff2fce8392e65df31f468
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-03-09 00:46:16 +0200

    final

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,25 +3,10 @@ import throttle from 'lodash.throttle';
 const currentTime = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
+const player = new Player(iframe);
 player.on('timeupdate', throttle(onStart, 1000));
 
 function onStart({ seconds }) {
   localStorage.setItem(currentTime, seconds);
 }
 player.setCurrentTime(localStorage.getItem(currentTime));
-
-// const player = new Player('handstick', {
-//   id: 19231868,
-//   width: 640,
-// });
-
-// const onPlay = function (data) {
-//   console.log(data);
-// };
-
-// const changeTime = () => {};
-
-// player.on('play', onPlay);
-// player.on('timeupdate', changeTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const formTextArea = document.querySelector(`textarea`);
+const form = document.querySelector(`form`);
+const emailLabel = document.querySelector(`input`);
+const STORAGE_KEY = `feedback-form-state`;
+const formData = {};
+//
+const populateTextArea = evt => {
+  const messageToSave = localStorage.getItem(STORAGE_KEY);
+  const parcedMessage = JSON.parse(messageToSave);
+  if (messageToSave) {
+    formTextArea.value = parcedMessage.message;
+  }
+};
+//
+populateTextArea();
+//
+const saveData = evt => {
+  const value = evt.target.value;
+  const name = evt.target.name;
+  formData[name] = value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+const submitForm = evt => {
+  evt.preventDefault();
+  const {
+    elements: { email },
+  } = evt.currentTarget;
+  if (email.value === '') {
+    return alert(`Please fill all fields`);
+  }
+  console.log(formData);
+  evt.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+form.addEventListener(`submit`, submitForm);
+form.addEventListener(`input`, throttle(saveData, 500));
