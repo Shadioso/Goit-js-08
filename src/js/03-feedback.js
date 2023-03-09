cf71fac8fda7a4979e722709b47b54832ff42fbb
Shadioso <114300237+Shadioso@users.noreply.github.com>
2023-03-09 19:51:54 +0200

    Help

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,40 @@
 import throttle from 'lodash.throttle';
 
-const formTextArea = document.querySelector(`textarea`);
+const formTextArea = document.querySelector(`.feedback-form textarea`);
 const form = document.querySelector(`form`);
-const emailLabel = document.querySelector(`input`);
+const emailLabel = document.querySelector(`.feedback-form input`);
 const STORAGE_KEY = `feedback-form-state`;
 const formData = {};
-//
-const populateTextArea = evt => {
-  const messageToSave = localStorage.getItem(STORAGE_KEY);
-  const parcedMessage = JSON.parse(messageToSave);
-  if (messageToSave) {
-    formTextArea.value = parcedMessage.message;
-  }
-};
-//
-populateTextArea();
-//
+
 const saveData = evt => {
   const value = evt.target.value;
   const name = evt.target.name;
   formData[name] = value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
-
+//
 const submitForm = evt => {
   evt.preventDefault();
   const {
-    elements: { email },
-  } = evt.currentTarget;
-  if (email.value === '') {
+    elements: { email, message },
+  } = evt.target;
+  if (email.value === '' && message.value === '') {
     return alert(`Please fill all fields`);
   }
-  console.log(formData);
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
   evt.target.reset();
   localStorage.removeItem(STORAGE_KEY);
 };
-
+//
+const populateTextArea = evt => {
+  const parcedMessageToSave = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parcedMessageToSave) {
+    formTextArea.value = parcedMessageToSave.message;
+    emailLabel.value = parcedMessageToSave.email;
+  }
+};
+//
+populateTextArea();
+//
 form.addEventListener(`submit`, submitForm);
 form.addEventListener(`input`, throttle(saveData, 500));
